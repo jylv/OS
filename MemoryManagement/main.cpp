000db149fe346bea949aa9bf9cbd7f2e46d7786f
jylv <jiaying_lyu@126.com>
2024-05-04 14:34:24 -0500

    add allocateMemory()

--- MemoryManagement/main.cpp
+++ MemoryManagement/main.cpp
@@ -1,4 +1,34 @@
 #include <iostream>
+#include <list>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+//Number of page frames in physical memory
+const int NUM_FRAMES = 4;
+//Size of each page
+const int PAGE_SIZE = 1024;
+//Frame list used for LRU page replacement
+list<int> frameList;
+//Used to quickly find the position of a page in the frame list
+unordered_map<int,list<int>::iterator> pageMap;
+//Physical memory
+vector<char> physicalMemory(NUM_FRAMES*PAGE_SIZE);
+
+int allocateMemory(int processId, int size) {
+    if(size>PAGE_SIZE) {
+        cerr<<"Requested size exceeds page size!"<<endl;
+        return -1;
+    }
+    //Find free frames or perform page replacement
+    list<int>::iterator it;
+    if(frameList.empty()) {
+        it = frameList.begin();
+
+    }
+}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
